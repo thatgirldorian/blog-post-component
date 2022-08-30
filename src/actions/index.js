9bd015ef54ebcdeaf58b82129f210aa01423ec11
Debbie Otuagomah <thatgirldorian@gmail.com>
2022-08-30 22:29:47 +0400

    fixed action creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,11 @@
+import jsonPlaceholder from '../apis/jsonPlaceholder'
+
 //wire up an action creator 
 export const fetchPosts = () => {
+    const promise = jsonPlaceholder.get('/posts')
+
     return {
-        type: 'FETCH_POST',
+        type: 'FETCH_POSTS',
+        payload: promise
     }
 }
