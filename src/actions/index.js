727a83b273226828c036a3f028f04d34c047947d
Debbie Otuagomah <thatgirldorian@gmail.com>
2022-09-06 00:43:51 +0400

    fixed the network request overflow

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,11 @@
 import _ from 'lodash'
 import jsonPlaceholder from '../apis/jsonPlaceholder'
 
+export const fetchPostsAndUsers = () => async dispatch => {
+        console.log('About to fetch posts')
+        await dispatch(fetchPosts())
+        console.log('Just fetched posts, yaay!')
+}
 
 //wire up an action creator to fetch posts
 export const fetchPosts = () => async dispatch =>  {
@@ -11,12 +16,15 @@ export const fetchPosts = () => async dispatch =>  {
 
 //add an action creator to fetch users
 export const fetchUser = (id) => async dispatch => {
-        _fetchUser(id, dispatch)
-}     
-
-//memoize the action creator with a private function 
-const _fetchUser = _.memoize(async (id, dispatch) => {
         const response = await jsonPlaceholder.get(`/users/${id}`)
 
-        dispatch({ type: 'FETCH_USER', payload: response.data })
-})
+        dispatch( {type: 'FETCH_USER', payload: response.data})
+}     
+
+
+// memoize the action creator with a private function 
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+//         const response = await jsonPlaceholder.get(`/users/${id}`)
+
+//         dispatch({ type: 'FETCH_USER', payload: response.data })
+// })
