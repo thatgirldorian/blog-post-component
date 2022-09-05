17d2258bfd4562594e5b4c77879094898d4444bd
Debbie Otuagomah <thatgirldorian@gmail.com>
2022-09-06 00:55:48 +0400

    Added a way to find unique user ids

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,12 @@
 import _ from 'lodash'
 import jsonPlaceholder from '../apis/jsonPlaceholder'
 
-export const fetchPostsAndUsers = () => async dispatch => {
-        console.log('About to fetch posts')
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
         await dispatch(fetchPosts())
-        console.log('Just fetched posts, yaay!')
+
+        //find all the unique user ids
+        const userIds = _.uniq(_.map(getState().posts, 'userId'))
+        userIds.forEach(id => dispatch(fetchUser(id)))
 }
 
 //wire up an action creator to fetch posts
